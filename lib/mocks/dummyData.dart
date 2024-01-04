a6fa0f2862dd839d03fb4fb9a256bcc9a42433c7
Mateo Nieto <matheo.developer@gmail.com>
2024-01-04 17:40:34 -0500

    first

---
+++ lib/mocks/dummyData.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+const DUMMY_CATEGORY = [
+  Category(id: 1, name: 'lacteos', color: Colors.blueGrey),
+  Category(id: 2, name: 'carnes', color: Colors.redAccent),
+  Category(id: 3, name: 'carnes frias', color: Colors.red),
+  Category(id: 4, name: 'otros', color: Colors.teal),
+];
